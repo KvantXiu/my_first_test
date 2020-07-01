d32f6e740f000372e959351e48f4cb95ea2014ea
KvantXiu <67580388+KvantXiu@users.noreply.github.com>
2020-07-01 09:35:43 +0800

    a = a+1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my_first_test
 hello world
+This is a C code.
+The output is "Hello world!". 
---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	a = a + 1;
+	printf("Hello world!\n");
+	printf("%d\n", a);
+	return 0;
+}
